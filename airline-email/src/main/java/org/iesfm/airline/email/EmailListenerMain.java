9c07dba6202c98f45177402dfe4b7e7e20872671
dapalo <davidskull94@gmail.com>
2022-02-08 16:02:35 +0100

    mongo y mq hecho

--- airline-email/src/main/java/org/iesfm/airline/email/EmailListenerMain.java
+++ airline-email/src/main/java/org/iesfm/airline/email/EmailListenerMain.java
@@ -1,7 +1,9 @@
 package org.iesfm.airline.email;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class EmailListenerMain {
     public static void main(String[] args) {
         SpringApplication.run(EmailListenerMain.class, args);
